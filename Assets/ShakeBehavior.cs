c88ee06144140c716e72c6e5dea09e57cc3ea85c
Mashandco <83590845+Mashandco@users.noreply.github.com>
2024-01-26 21:56:03 +0100

    Add Shake Behavior
    Added first scrit shake behavior

---
+++ Assets/ShakeBehavior.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeBehavior : MonoBehaviour
+{
+    // Transform of the GameObject you want to shake
+    public Transform transform;
+
+    // Desired duration of the shake effect
+    public float shakeDuration = 0f;
+
+    // A measure of magnitude for the shake
+    public float shakeMagnitude = 0.7f;
+
+    // Shake damping speed 
+    public  float dampingSpeed = 1.0f;
+
+    // The initial position of the GameObject
+    private Vector3 initialPosition;
+
+    void Awake()
+    {
+        if (transform == null)
+        {
+            transform = this.GetComponent<Transform>();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + (Vector3) Random.insideUnitCircle * shakeMagnitude;
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+
+    public void TriggerShake()
+    {
+        shakeDuration = 2.0f;
+    }
+}
