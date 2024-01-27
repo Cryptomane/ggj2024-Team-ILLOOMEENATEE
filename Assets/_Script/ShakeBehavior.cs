2512e2414e873748ed1165ac733d9e2fb147012b
Sergio Bonfiglio <bonfiglio.ser@gmail.com>
2024-01-27 10:52:01 +0100

    Modifiche allo shakebehaviour

--- Assets/_Script/ShakeBehavior.cs
+++ Assets/_Script/ShakeBehavior.cs
@@ -4,41 +4,22 @@ using UnityEngine;
 
 public class ShakeBehavior : MonoBehaviour
 {
-    // Transform of the GameObject you want to shake
-    public Transform originalTransform;
-
-    // Desired duration of the shake effect
-    public float shakeDuration = 0f;
-
-    // A measure of magnitude for the shake
-    public float shakeMagnitude = 0.7f;
-
     // Shake damping speed 
     public float dampingSpeed = 1.0f;
-
-    // Start time 
-    public float startTime;
-    public float timeout = 2.0f;
 
     // The initial position of the GameObject
     private Vector3 initialPosition;
 
-    public ShakeBehavior(float shakeDuration)
-    {
-        this.shakeDuration = shakeDuration;
-    }
+    // Desired duration of the shake effect
+    private float shakeDuration = 0f;
 
-    void Awake()
-    {
-        if (originalTransform == null)
-        {
-            originalTransform = this.GetComponent<Transform>();
-        }
-    }
+    // A measure of magnitude for the shake
+    private float shakeMagnitude = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        initialPosition = originalTransform.localPosition;
+        initialPosition = transform.localPosition;
     }
 
     // Update is called once per frame
@@ -46,19 +27,19 @@ public class ShakeBehavior : MonoBehaviour
     {
         if (shakeDuration > 0)
         {
-            originalTransform.localPosition = initialPosition + (Vector3)Random.insideUnitCircle * shakeMagnitude;
+            transform.localPosition = initialPosition + (Vector3)Random.insideUnitCircle * shakeMagnitude;
             shakeDuration -= Time.deltaTime * dampingSpeed;
         }
         else
         {
             shakeDuration = 0f;
-            originalTransform.localPosition = initialPosition;
+            transform.localPosition = initialPosition;
         }
     }
 
-    public void TriggerShake()
+    public void TriggerShake(float duration, float magnitude)
     {
-        startTime=Time.time;
-        shakeDuration = Mathf.Lerp( .5f, timeout,(Time.time - startTime)/60f);
+        shakeDuration = duration;
+		shakeMagnitude = magnitude;
     }
 }
