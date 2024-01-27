61c551a43d9abdcdd15b6ec013132c72c247c250
Sergio Bonfiglio <bonfiglio.ser@gmail.com>
2024-01-27 14:36:31 +0100

    no message

--- Assets/_Script/ControlsScreen.cs
+++ Assets/_Script/ControlsScreen.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class ControlsScreen : MonoBehaviour
 {
-	public void Init(GameObject CommandsPrefab)
+	public void Show(GameObject CommandsPrefab)
 	{
 		GameObject instance = GameObject.Instantiate(CommandsPrefab, transform, false);
 
 		instance.transform.localPosition = Vector3.zero;
 		gameObject.SetActive(true);
+	}
+
+	public void Hide()
+	{
+		Destroy(transform.GetChild(0));
+		gameObject.SetActive(false);
 	}
 }
--- Assets/_Script/MinigameManager.cs
+++ Assets/_Script/MinigameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,76 +10,106 @@ public class MinigameManager : MonoBehaviour
 	{
 		INTRO,          // Introduce round
 		TRANSITION,     // Transition between states
-		COMMANDS,       // Introduce commands
+		CONTROLS,       // Introduce commands
 		MINIGAME,       // Minigame section start
-		RESULT          // Result logic that select next minigame, round or game over
+		RESULT,          // Result logic that select next minigame, round or game over
+		GAME_OVER          // Result logic that select next minigame, round or game over
 	}
 
 	[SerializeField] private Animator m_TransitionAnimator;
 
 	[Header("Data")]
-    [SerializeField] private MinigameSelection m_Minigames;
+	[SerializeField] private MinigameSelection m_Minigames;
 
 	[Header("Difficulty Settings")]
-    [SerializeField] private float m_Difficulty = 1.0f;
-    [SerializeField] private float m_IncreaseFactorDifficulty = 0.5f;
-	
+	[SerializeField] private float m_Difficulty = 1.0f;
+	[SerializeField] private float m_IncreaseFactorDifficulty = 0.5f;
+
 	[Header("Game Elements")]
 	[SerializeField] private GameObject m_RoundIntro;
 	[SerializeField] private ControlsScreen m_Commands;
 	[SerializeField] private GameObject m_Result;
 
-    private Minigame m_CurrentMinigame;
+	private static event Action m_OnGameStartRequested;
+
+	public static event Action OnGameStartRequested
+	{
+		add
+		{
+			m_OnGameStartRequested -= value;
+			m_OnGameStartRequested += value;
+		}
+		remove
+		{
+			m_OnGameStartRequested -= value;
+		}
+	}
+
+	private Minigame m_CurrentMinigame;
 	private GameState m_CurrentState;
 	private GameState m_NextState;
 	private bool m_Transitioning;
+	private float m_Timer;
 
 	private int m_TransitionStartAnimationTrigger = Animator.StringToHash("start");
 	private int m_TransitionEndAnimationTrigger = Animator.StringToHash("end");
-    
- private void Update()
-    {
-        switch (m_CurrentState)
-        {
+
+	private void Update()
+	{
+		switch (m_CurrentState)
+		{
 			case GameState.TRANSITION:
-				if(!m_Transitioning)
+				if (!m_Transitioning)
 				{
 					m_CurrentState = m_NextState;
 				}
 				break;
-            case GameState.INTRO:
-                if (Input.anyKey)
-                {
-					m_NextState = GameState.COMMANDS;
-					m_CurrentState = GameState.TRANSITION;
-					m_Transitioning = true;
+			case GameState.INTRO:
+				if (Input.anyKey)
+				{
 					m_Minigames.InitRound();
-                    ShowCommandsScreen();
-                }
-                break;
-        case GameState.COMMANDS:
-                if (Input.anyKey)
-                {
+					ShowControlsScreen();
+				}
+				break;
+			case GameState.CONTROLS:
+				if (Input.anyKey)
+				{
+					StartMinigame();
+				}
+				break;
+			case GameState.MINIGAME:
+				if (m_Timer <= 0)
+				{
+				}
+				m_Timer -= Time.deltaTime;
 
-                }
-                break;
-        case GameState.MINIGAME: 
-                break;
-        case GameState.RESULT: 
-                break;
-        default: 
-                break;
-        }
-        
-        startMinigame(m_CurrentMinigame.Duration);
-    }
+				break;
+			case GameState.RESULT:
+				break;
+			case GameState.GAME_OVER:
+				break;
+			default:
+				break;
+		}
+	}
 
-    public void ShowCommandsScreen()
-    {
+	public void ShowControlsScreen()
+	{
+		m_NextState = GameState.CONTROLS;
+		m_CurrentState = GameState.TRANSITION;
+		m_Transitioning = true;
 		m_CurrentMinigame = m_Minigames.GetNextMinigame();
 		StartCoroutine(LoadMinigameScene(m_CurrentMinigame.SceneName));
-		m_Commands.Init(m_CurrentMinigame.InstructionsControlsPrefab);
-    }
+		m_Commands.Show(m_CurrentMinigame.InstructionsControlsPrefab);
+	}
+
+	private void StartMinigame()
+	{
+		m_Timer = m_CurrentMinigame.Duration;
+		m_CurrentState = GameState.MINIGAME;
+		m_Commands.Hide();
+		m_OnGameStartRequested?.Invoke();
+	}
 
 	private IEnumerator LoadMinigameScene(string sceneName)
 	{
@@ -87,7 +118,7 @@ public class MinigameManager : MonoBehaviour
 		yield return new WaitForSecondsRealtime(0.7f);
 		AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
 
-		while(!loading.isDone)
+		while (!loading.isDone)
 		{
 			yield return new WaitForEndOfFrame();
 		}
@@ -95,20 +126,4 @@ public class MinigameManager : MonoBehaviour
 		m_TransitionAnimator.SetTrigger(m_TransitionEndAnimationTrigger);
 		m_Transitioning = false;
 	}
-
-    IEnumerator startMinigame(float seconds)
-    {
-        float time = seconds;
-        while (time > 0)
-        {
-            yield return new WaitForSeconds(1);
-            time--;
-        }
-        changeStateHandler(GameState.RESULT);
-    }
-
-    private void changeStateHandler(GameState nextState) 
-    {
-        m_CurrentState = nextState;
-    }
 }
